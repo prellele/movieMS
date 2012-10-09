9f2c4fcca41f728e68c9446e174f94d2309de069
Lennart Prelle <me@lennartprelle.de>
2012-10-09 20:00:09 +0200

    remove resolution also from model, controller and view /:

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,18 @@
 module MoviesHelper
 
-  def order_by_links(label, column)
+  def order_by_links
+    links = ""
+    {"Name" => "name", "created_at" => "created at"}.each do |column, label|
+      if params[:order] == column
+        links += content_tag(:span, "Order by #{label}")
+      else
+        links += link_to label, list_movies_path(:order => column)
+      end
+    end
+    links.html_safe
+  end
+
+  def order_by_by_links(label, column)
     (column == params[:oder] ? content_tag(:span, label) : link_to(label, list_movies_path(:order => column))).html_safe
   end
   

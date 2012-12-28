33b7017151cd3530f996a86bd04f7307613b02c4
Lennart Prelle <me@lennartprelle.de>
2012-12-28 22:29:36 +0100

    correct selected column, update select color

--- app/assets/javascripts/selector.js
+++ app/assets/javascripts/selector.js
@@ -1,5 +1,5 @@
 $('.movierow .selectable').click(function () {
     var movieid = $(this).parent().data("id");
     $.post('/movies/' + movieid + '/favorite', {"create": ($(this).parent().hasClass("selected") ? false : true )});
-    $(this).parent().toggleClass('selected');
+    $(this).parent().find(".selectable").toggleClass('selected');
 });
